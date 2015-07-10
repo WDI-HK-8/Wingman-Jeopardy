aa09d1d598bb54405528aa6f4e3654161af27f3d
Carson Trent Long <CarsonLong@IOS-Syncy.local>
2015-07-10 17:15:10 +0800

    initialized application.js and made the question/answer array

---
+++ js/application.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	var array = []
+  array[0] = {question : "Your Bro is super drunk and has thrown up, the girl he likes shows up, do you...", answer1 : 'Grab him and run.', answer2 : 'Say it\'s your throw up.', answer3 : 'Tell her that she looks beautiful.', best : 'answer2', okay : 'answer1', bad : 'answer3'};
+  
+
+})
